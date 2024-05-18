8618f0e7eb3c3f8d7c55a68091fb791576dffd9d
Juan-Pedro-Kessler <juanpedrokessler@gmail.com>
2024-05-18 16:23:11 -0300

    primero

---
+++ tp4/punto 1.cpp
@@ -0,0 +1,11 @@
+//Escribir un programa que muestre los números del 1 al 10 utilizando un bucle while.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	while (a <= 10){
+		cout << a << endl;
+		a++;
+	}
+}
