5070c23d3a9e48db08ead4847f86d7676ee3aab3
Juan-Pedro-Kessler <juanpedrokessler@gmail.com>
2024-05-18 17:29:10 -0300

    segundo

---
+++ tp4/punto 2.cpp
@@ -0,0 +1,15 @@
+// Escribir un programa que pida al usuario un número y muestre su tabla de multiplicar utilizando un bucle for.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rt, a;
+    cout << "ingresa un numero: \n";
+    cin >> rt;
+    for(a=1; a<=10; a++){
+        cout << "el resultado de " << rt << "x" << a << " es igual a= " << rt * a << endl;
+    }
+    cout << "programa finalizado..." << endl;
+}
+
