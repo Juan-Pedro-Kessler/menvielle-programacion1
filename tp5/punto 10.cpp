feb1a199b87ddcbf91d940605bed8767af76a6b7
Juan-Pedro-Kessler <juanpedrokessler@gmail.com>
2024-05-20 21:31:39 -0300

    LOL

--- tp5/punto 10.cpp
+++ tp5/punto 10.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main()
-{
-    int i, j;
-    char matriz[5][5], rt;
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
+
+int main() {
+    const int n = 5;
+    int i, j, rt;
+    int matrix[n][n];
+
+    // Rellenar la matriz
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             if (i == j) {
-                cout << "elegi un numero para ese espacio \n";
+                cout << "Elige un numero para ese espacio: ";
                 cin >> rt;
-                matriz[i][j] = rt;
+                matrix[i][j] = rt;
             }
-            else { matriz[i][j] = 'x'; }
+            else {
+                matrix[i][j] = -1; // Usar -1 para marcar espacios no inicializados
+            }
         }
     }
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            cout << matriz[i][j] << "\t";
+
+    // Mostrar la matriz
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] == -1) {
+                cout << 'x' << "\t"; // Mostrar 'x' para los espacios no inicializados
+            }
+            else {
+                cout << matrix[i][j] << "\t";
+            }
         }
         cout << endl;
     }
-            
 
+    return 0;
 }
