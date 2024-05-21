3685ea8e14b11c1ee99084934f47bc38f1eb75b2
Juan-Pedro-Kessler <102627746+Juan-Pedro-Kessler@users.noreply.github.com>
2024-05-21 16:32:35 -0300

    octavo

--- tp5/punto i.cpp
+++ tp5/punto i.cpp
@@ -2,40 +2,68 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int main()
-{
-    int i = 0, j = 0, a = 0, rt, matriz[10][10];
-    char opc='y';
+
+int main() {
+    int i = 0, j = 0, a = 0, rt, matriz[10][10], arreglo[10], total = 0;
+    char opc = 'y';
     bool d = false;
     srand(time(0));
+
+
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             matriz[i][j] = rand() % 99 + 1;
         }
     }
-    
-    for(i = 0; i < 10; i++) {
+
+
+    for (i = 0; i < 10; i++) {
+        total = 0;  
         for (j = 0; j < 10; j++) {
-            cout << matriz[i][j]<< "\t";
+            total += matriz[i][j];
+        }
+        arreglo[i] = total;
+    }
+
+ 
+    for (a = 0; a < 10; a++) {
+        cout << "Suma de la fila " << a + 1 << ": " << arreglo[a] << endl;
+    }
+
+
+    cout << "Contenido de la matriz:" << endl;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            cout << matriz[i][j] << "\t";
         }
         cout << endl;
     }
+
     while (opc == 'y') {
-        cout << "que numero deseas buscar?\n";
+        cout << "¿Qué número deseas buscar?\n";
         cin >> rt;
-        for (i = 0; i < 10 && d==false; i++) {
-            for (j = 0; j < 10 && d==false; j++) {
+
+        d = false; 
+
+        for (i = 0; i < 10 && !d; i++) {
+            for (j = 0; j < 10 && !d; j++) {
                 if (rt == matriz[i][j]) {
                     d = true;
-                    break;}
-                else { d=false; };
+                    break;
+                }
             }
-        
         }
-        if (d == true) { cout << "el numero se encuentra en la matriz\n"; }
-        else { cout << "el numero NO se encuentra en la matriz\n"; };
-        cout << "deseas reintentar con otro numero?\n";
+
+        if (d) {
+            cout << "El número se encuentra en la matriz\n";
+        }
+        else {
+            cout << "El número NO se encuentra en la matriz\n";
+        }
+
+        cout << "¿Deseas reintentar con otro número? (y/n)\n";
         cin >> opc;
-        if (opc == 'y') { d = false; };
     }
+
+    return 0;
 }
