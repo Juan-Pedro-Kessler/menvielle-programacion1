8dce2629145412a23bd9cefa129bd6131fdb67f5
Juan-Pedro-Kessler <juanpedrokessler@gmail.com>
2024-06-09 15:24:42 -0300

    segundo

---
+++ tp6/punto b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Función para calcular la suma de los elementos de un arreglo
+int suma(int c, int arre[]) {
+    int total = 0;
+    for (int d = 0; d < c; d++) {
+        total += arre[d];
+    }
+    return total; // Retornamos el total en lugar de imprimirlo
+}
+
+int main() {
+    int a;
+    cout << "Introduce el tamaño del arreglo: ";
+    cin >> a;
+
+    int arreglo[a];
+    cout << "Introduce los elementos del arreglo: ";
+    for (int b = 0; b < a; b++) {
+        cin >> arreglo[b];
+    }
+
+    // Llamamos a la función suma pasando el arreglo completo
+    int resultado = suma(a, arreglo);
+
+    // Imprimimos el resultado
+    cout << "La suma de los elementos del arreglo es: " << resultado << endl;
+
+    return 0;
+}
